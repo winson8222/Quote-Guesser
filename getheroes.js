36f8eae52e94f6150384b1049c00c46fd835e096
Winson8222 <winson8222@hotmail.com>
2023-01-02 16:21:53 +0800

    "fourth try"

--- getheroes.js
+++ getheroes.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 async function getQuote() {
     const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox','--disable-setuid-sandbox']
+        args: ['--no-sandbox']
     });
     const page = await browser.newPage();
 
