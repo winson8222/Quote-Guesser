faa59642811763ab95394c3656685ce19ef942ae
Winson8222 <winson8222@hotmail.com>
2023-01-02 21:12:55 +0800

    "pango test"

--- public/js/getQuote.js
+++ public/js/getQuote.js
@@ -17,6 +17,7 @@ async function getQuote() {
             choices[i] = heroes.randomHeroes(hList);
         }
         var correct = heroes.getCorrect(choices);
+        correct.hero = "Pangolier";
         console.log(correct.hero);
 
 
