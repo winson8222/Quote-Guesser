46a0b12fdc26e3c795e613cfa82c0e183c7ef8ed
Winson8222 <winson8222@hotmail.com>
2023-01-02 15:58:55 +0800

    Fixing deployment issue

--- getheroes.js
+++ getheroes.js
@@ -1,7 +1,12 @@
 const puppeteer = require("puppeteer");
 
 async function getQuote() {
-    const browser = await puppeteer.launch({})
+    const browser = await puppeteer.launch({
+        'args' : [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+          ]
+    });
     const page = await browser.newPage();
 
     await page.goto("https://dota2.fandom.com/wiki/Heroes_by_release");
