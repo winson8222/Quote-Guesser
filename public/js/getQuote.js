f5c093ab59f6e1bcad28e3e821779c7476d591a1
Winson8222 <winson8222@hotmail.com>
2023-01-02 22:00:48 +0800

    "v1.00 (no non li6 heroes)"

--- public/js/getQuote.js
+++ public/js/getQuote.js
@@ -17,7 +17,6 @@ async function getQuote() {
             choices[i] = heroes.randomHeroes(hList);
         }
         var correct = heroes.getCorrect(choices);
-        correct.hero = 'Centaur_Warrunner';
         console.log(correct.hero);
 
 
